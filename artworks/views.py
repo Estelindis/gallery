d6b52071f05a190a06380c788147a16c6fdcbd30
Estelindis <estelindis@gmail.com>
2023-03-09 20:45:08 +0000

    Add artwork details functionality

--- artworks/views.py
+++ artworks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Artwork
 
 # Create your views here.
+
 
 def all_artworks(request):
     """ A view to show all artworks, incl. sort & search """
@@ -13,3 +14,15 @@ def all_artworks(request):
     }
 
     return render(request, 'artworks/artworks.html', context)
+
+
+def artwork_detail(request, artwork_id):
+    """ A view to show individual artwork details """
+
+    artwork = get_object_or_404(Artwork, pk=artwork_id)
+
+    context = {
+        'artwork': artwork,
+    }
+
+    return render(request, 'artworks/artwork_detail.html', context)
