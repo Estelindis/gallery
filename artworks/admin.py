a19da8a8bc52f4cfef9185fdfb63fb69a4d96c27
Estelindis <estelindis@gmail.com>
2023-03-09 17:42:30 +0000

    Customize admin with friendly names + other fields

--- artworks/admin.py
+++ artworks/admin.py
@@ -2,7 +2,55 @@ from django.contrib import admin
 from .models import Artist, Canvas, Design, Artwork
 
 # Register your models here.
-admin.site.register(Artist)
-admin.site.register(Canvas)
-admin.site.register(Design)
-admin.site.register(Artwork)
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'sku_code',
+        'name',
+        'full_name',
+    )
+
+    ordering = ('friendly_name',)
+
+
+class CanvasAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'sku_code',
+        'name',
+        'price',
+    )
+
+    ordering = ('friendly_name',)
+
+
+class DesignAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'sku_code',
+        'name',
+        'artist',
+    )
+
+    ordering = ('friendly_name',)
+
+
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'friendly_name',
+        'name',
+        'artist',
+        'design',
+        'canvas',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Canvas, CanvasAdmin)
+admin.site.register(Design, DesignAdmin)
+admin.site.register(Artwork, ArtworkAdmin)
--- artworks/models.py
+++ artworks/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Artist(models.Model):
     """
     Model for the artists selling artworks on the site.
+
+    SKU Code should be a short string, preferably all caps.
+    SKU Codes from Artist, Canvas, and Design are
+    concatenated to render the Artwork SKU.
     """
     name = models.CharField(max_length=80)
-    sku_code = models.CharField(max_length=6)
+    sku_code = models.CharField(
+        max_length=6,
+        verbose_name="SKU Code")
     friendly_name = models.CharField(
         max_length=80,
         verbose_name="Friendly Name")
@@ -82,9 +88,15 @@ class Artist(models.Model):
 class Canvas(models.Model):
     """
     Model for the types of product ("canvas") on which designs can be printed.
+
+    SKU Code should be a short string, preferably all caps.
+    SKU Codes from Artist, Canvas, and Design are
+    concatenated to render the Artwork SKU.
     """
     name = models.CharField(max_length=80)
-    sku_code = models.CharField(max_length=12)
+    sku_code = models.CharField(
+        max_length=12,
+        verbose_name="SKU Code")
     friendly_name = models.CharField(
         max_length=80,
         verbose_name="Friendly Name")
@@ -117,9 +129,15 @@ class Canvas(models.Model):
 class Design(models.Model):
     """
     Model for the designs that can be printed on various canvasses.
+
+    SKU Code should be a short string, preferably all caps.
+    SKU Codes from Artist, Canvas, and Design are
+    concatenated to render the Artwork SKU.
     """
     name = models.CharField(max_length=80)
-    sku_code = models.CharField(max_length=12)
+    sku_code = models.CharField(
+        max_length=12,
+        verbose_name="SKU Code")
     friendly_name = models.CharField(
         max_length=80,
         verbose_name="Friendly Name")
@@ -153,12 +171,15 @@ class Artwork(models.Model):
     """
     Model for the individual products ("artworks") sold on the site.
 
+    SKU is concatenated from Artist, Canvas, and Design.
     Some artwork info is stored in other models, e.g. Canvas and Design.
     This reduces duplication of info and allows centralised updates.
     For instance, the dimensions of each mug will always be the same.
     If the mug price changes, that is automatically applied to all mugs.
     """
-    sku = models.CharField(max_length=40)
+    sku = models.CharField(
+        max_length=40,
+        verbose_name="SKU")
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(
         max_length=250,
