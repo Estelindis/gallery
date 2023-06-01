9c56f373003bf4b54febdec0c83f6aa91e68af95
Estelindis <estelindis@gmail.com>
2023-06-01 01:21:43 +0000

    Add checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -47,7 +47,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(
-            Sum('lineitem_total'))['lineitem_total__sum']
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = (
                 self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -74,7 +74,7 @@ class OrderLineItem(models.Model):
         Order,
         null=False,
         blank=False,
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE, 
         related_name='lineitems')
     artwork = models.ForeignKey(
         Artwork,
